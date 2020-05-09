804271fd8fce5b44a797a4670c6b540f49d1c8d8
Cristian Felipe Gallego Ramírez <pharsat@hotmail.com>
2020-05-09 16:09:47 -0500

    Add subFolder Component

---
+++ src/components/SubFolder.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="subFolder">
+    <span v-show="showName" @click="showNameInput">{{name}}</span>
+    <input v-show="!showName" type="text" v-model="name" @keyup.enter="showNameInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SubFolder",
+  props: [],
+  data() {
+    return {
+      showName: false,
+      showText: false
+    };
+  },
+  methods: {
+    showNameInput() {
+      this.showName = !this.showName;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.file {
+  width: 100%;
+  background-color:chartreuse;
+}
+</style>>
